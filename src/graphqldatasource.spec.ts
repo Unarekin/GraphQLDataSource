cbe352559fbd34cbff33474228a17b37290dfc31
Erica Phelps <erica@blackspork.com>
2020-10-31 19:46:31 -0500

    Test setup

---
+++ src/graphqldatasource.spec.ts
@@ -0,0 +1,13 @@
+import { TestBed } from '@angular/core/testing';
+import { GraphQLDataSource } from './graphqldatasource';
+
+describe('GraphQLDataSource', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({});
+
+  })
+
+  it("Should be created", () => {
+
+  });
+})
