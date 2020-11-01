b9e3358907c94c1c11419c82e7e23741b7438b45
Erica Phelps <erica@blackspork.com>
2020-10-31 19:27:03 -0500

    Shifted over to just pure class, as it doesn't need to be imported or any damn thing.

--- projects/ngx-graphqldatasource/src/public-api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of ngx-graphqldatasource
- */
-
-export * from './lib/ngx-graphqldatasource.service';
-export * from './lib/ngx-graphqldatasource.component';
-export * from './lib/ngx-graphqldatasource.module';
